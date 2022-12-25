fb4e02f71cb5234fe6d3cc6b80a22bb5e70ca486
Ashutosh Dave <ashutosh.dave@eternussolutions.com>
2022-12-25 14:01:09 +0530

    Initial Commit

---
+++ ProductListSwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProductListSwiftUIDemo
+//
+//  Created by ashu on 25/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProductListSwiftUIDemo/ProductListSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductListSwiftUIDemoApp.swift
+//  ProductListSwiftUIDemo
+//
+//  Created by ashu on 25/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ProductListSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
